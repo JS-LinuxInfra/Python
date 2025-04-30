6ba5261d687ff68cdacee9076d8ff767c8e48c65
Justin Sniesak <justindsniesak@gmail.com>
2025-04-30 14:51:28 -0500

    Create multi_line_string.py

---
+++ multi_line_string.py
@@ -0,0 +1,15 @@
+# Assign the string here
+to_you = """
+Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you?
+"""
+
+#Prints the multi line string
+print(to_you)
+
+Output:
+
+Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you?
