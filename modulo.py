c96038f2562f908265bc2edca0a92ad17a0be480
Justin Sniesak <justindsniesak@gmail.com>
2025-04-30 14:45:05 -0500

    Create modulo.py

---
+++ modulo.py
@@ -0,0 +1,14 @@
+# First order of the day
+first_order_remainder = 269 % 10
+print(first_order_remainder)
+# Determines if order should get a coupon
+first_order_coupon = "no"
+# Second order of the day
+second_order_remainder = 270 % 10
+print(second_order_remainder)
+# Determines if order should get a coupon
+second_order_coupon = "yes"
+
+Output:
+9
+0
