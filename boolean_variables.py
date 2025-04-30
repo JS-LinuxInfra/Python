3e180e06b4e0c41dded27e0ee51a8d734949f88c
Justin Sniesak <justindsniesak@gmail.com>
2025-04-30 15:46:49 -0500

    Create boolean_variables.py

---
+++ boolean_variables.py
@@ -0,0 +1,12 @@
+my_baby_bool = True
+print(my_baby_bool)
+print(type(my_baby_bool))
+my_baby_bool_two=True
+print(my_baby_bool_two)
+print(type(my_baby_bool_two))
+
+Output:
+True
+<class 'bool'>
+True
+<class 'bool'>
