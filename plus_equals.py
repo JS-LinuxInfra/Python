a46919541cfee7fe9034bd5e5791912bb19c1dbb
Justin Sniesak <justindsniesak@gmail.com>
2025-04-30 14:49:09 -0500

    Create plus_equals.py

---
+++ plus_equals.py
@@ -0,0 +1,15 @@
+total_price = 0
+
+new_sneakers = 50.00
+nice_sweater = 39.00
+fun_books = 20.00
+
+total_price += new_sneakers
+total_price += nice_sweater
+total_price += fun_books
+
+# Update total_price here:
+
+print("The total price is", total_price)
+
+The total price is 109.0
