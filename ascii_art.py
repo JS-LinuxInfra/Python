9c552b688ef49c5a0df1b7ea1fe2258a1012d811
Justin Sniesak <justindsniesak@gmail.com>
2025-04-30 14:53:56 -0500

    Create ascii_art.py

---
+++ ascii_art.py
@@ -0,0 +1,21 @@
+# My name is Justin Sniesak
+# This will define my first and last initial using ASCII characters
+first_last_letters="""
+J J J J J      S S S
+    J        S       S
+    J        S
+    J          S S S
+J   J                S
+J   J        S       S
+  J J          S S S
+"""
+#This will print the results
+print(first_last_letters)
+
+J J J J J      S S S
+    J        S       S
+    J        S
+    J          S S S
+J   J                S
+J   J        S       S
+  J J          S S S
