84e9c3f0801ebdd973e7b902463186dd76ec4c0a
Justin Sniesak <justindsniesak@gmail.com>
2025-04-30 19:02:54 -0500

    Create remove_list.py

---
+++ remove_list.py
@@ -0,0 +1,7 @@
+# Define list, elements, what to remove, print output to the terminal
+new_store_order_list = ["Orange", "Apple", "Mango", "Broccoli", "Mango"]
+new_store_order_list.remove("Broccoli")
+print(new_store_order_list)
+
+#Modified list output
+['Orange', 'Apple', 'Mango', 'Mango']
