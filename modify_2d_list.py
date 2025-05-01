c9364b07327a23b3b397ec6e2ee082b1027dce93
Justin Sniesak <justindsniesak@gmail.com>
2025-04-30 19:07:54 -0500

    Create modify_2d_list.py

---
+++ modify_2d_list.py
@@ -0,0 +1,7 @@
+#Create 2D list, modify elements and print output to the terminal
+incoming_class = [['Kenny', 'American', 9], ['Tanya', 'Ukrainian', 9], ['Madison', 'Indian', 7]]
+incoming_class[-3][-3] = "Ken"
+print(incoming_class) 
+
+Output:
+[['Ken', 'American', 9], ['Tanya', 'Ukrainian', 9], ['Madison', 'Indian', 7]]
