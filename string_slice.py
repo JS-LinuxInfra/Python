6343cc0603e97a9be0a8d8863a7c2fbfee67b09d
Justin Sniesak <justindsniesak@gmail.com>
2025-05-11 20:07:47 -0500

    Create string_slice.py

---
+++ string_slice.py
@@ -0,0 +1,9 @@
+first_name = "Rodrigo"
+last_name = "Villanueva"
+
+#Creates a UID with the first five letters of their last name, then prints to the terminal
+new_account = last_name[:5]
+print(new_account)
+#Creates a temp password containing the third to the sixth letters of their last name
+temp_password = last_name[2:6]
+print(temp_password)
