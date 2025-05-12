ed39307e0f4f86232858bdf5f7008ccfd379e908
Justin Sniesak <justindsniesak@gmail.com>
2025-05-11 20:38:40 -0500

    Create correct_string.py

---
+++ correct_string.py
@@ -0,0 +1,7 @@
+#Define variables
+first_name = "Bob"
+last_name = "Daily"
+
+#Correct first variable typo by creating new variable, assigning correct first letter, and concatenating correct slice of this variable to fix the variable
+fixed_first_name = "R" + first_name[1:]
+print(fixed_first_name) 
