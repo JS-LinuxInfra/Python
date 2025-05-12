262b3b89009d64d454d7b5877d6afb1b8a132029
Justin Sniesak <justindsniesak@gmail.com>
2025-05-11 20:31:47 -0500

    Create negative_indice.py

---
+++ negative_indice.py
@@ -0,0 +1,10 @@
+#Assign string to variable
+company_motto = "Copeland's Corporate Company helps you capably cope with the constant cacophony of daily life"
+
+#Use a negative indices to find the second to last character, and save to a variable. Print the result.
+second_to_last = company_motto[-2]
+print(second_to_last)
+
+#Use a negative indice to slice the last four characters of the company_motto variable, then save the result to a new variable
+final_word = company_motto[-4:]
+print(final_word)
