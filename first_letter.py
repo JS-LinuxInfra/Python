c046ddd33a6c5705f55cdaaa3251770aede0c747
Justin Sniesak <justindsniesak@gmail.com>
2025-05-11 20:05:45 -0500

    Create first_letter.py

---
+++ first_letter.py
@@ -0,0 +1,6 @@
+#Assigns a name to a variable
+my_name= "Justin"
+#Selects the first letter from the previous variable and assigns to a new variable
+first_initial = my_name[0]
+#Prints the second variable
+print(first_initial)
