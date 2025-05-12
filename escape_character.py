b4808c37c04a6ce206dc3c927a3750fe054d077a
Justin Sniesak <justindsniesak@gmail.com>
2025-05-11 20:43:28 -0500

    Create escape_character.py

---
+++ escape_character.py
@@ -0,0 +1,5 @@
+#Define initial variable, and correct using an escape charater to address quotes within a string
+password = "theycallme\"crazy\"91"
+
+#Print the corrected variable
+print(password)
