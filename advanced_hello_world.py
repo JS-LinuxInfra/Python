5324454befbb74dad9d18fc6f6b665198828d694
Justin Sniesak <justindsniesak@gmail.com>
2025-05-14 16:31:40 -0500

    Update advanced_hello_world.py

--- advanced_hello_world.py
+++ advanced_hello_world.py
@@ -4,12 +4,7 @@ half_my_age = my_age / 2
 greeting = "Hello,"
 name = "what is your name? "
 greeting_with_name_and_age = f"I'm Justin, I'm " + str(my_age) + " how are you doing today?"
-
-#Print out greeting, age, and return greeting
+#Print greeting and name
 print(greeting + " " + name) 
-
+#Print greeting, name and age
 print(greeting_with_name_and_age)
-
-
-Hello, what is your name? 
-I'm Justin, I'm 42 how are you doing today?
