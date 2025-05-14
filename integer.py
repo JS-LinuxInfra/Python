c40b466a3d6b037a5000122c758b8bf37fc8ba2a
Justin Sniesak <justindsniesak@gmail.com>
2025-05-14 16:34:38 -0500

    Update integer.py

--- integer.py
+++ integer.py
@@ -1,13 +1,11 @@
-# Define the release and runtime integer variables below:
-#release year
+#Define the release and runtime integer variables below:
+#Release year
 release_year = 6
-#runtime
+#Runtime
 runtime = 9
-#rating
+#Rating
 rating_out_of_10 = 6.9
-#prints the output
+#Prints the output
 print(release_year + runtime + rating_out_of_10) 
-
-
 # Define the rating_out_of_10 float variable below: 
 21.9
