c85fbb533afe8823583a84e5c086e40c42fff26a
Justin Sniesak <justindsniesak@gmail.com>
2025-05-14 16:36:23 -0500

    Update exponents.py

--- exponents.py
+++ exponents.py
@@ -1,15 +1,9 @@
-# Calculation of squares for:
-# 6x6 quilt - 6 squared
+#Calculation of squares for:
+#6x6 quilt - 6 squared
 print(6 ** 2)
-# 7x7 quilt - 7 squared
+#7x7 quilt - 7 squared
 print(7 ** 2)
-# 8x8 quilt - 8 squared
+#8x8 quilt - 8 squared
 print(8 ** 2)
-# How many squares for 6 people to have 6 quilts each that are 6x6?
+#How many squares for 6 people to have 6 quilts each that are 6x6?
 print(6 * 6 ** 2 * 6)
-
-Output:
-36
-49
-64
-1296
