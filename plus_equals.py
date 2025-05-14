a812760d60ace5fe803ba577865c2b43227a90a0
Justin Sniesak <justindsniesak@gmail.com>
2025-05-14 16:38:18 -0500

    Update plus_equals.py

--- plus_equals.py
+++ plus_equals.py
@@ -1,3 +1,4 @@
+#Define vairables
 total_price = 0
 
 new_sneakers = 50.00
@@ -8,8 +9,5 @@ total_price += new_sneakers
 total_price += nice_sweater
 total_price += fun_books
 
-# Update total_price here:
-
+#Update the total_price here:
 print("The total price is", total_price)
-
-The total price is 109.0
