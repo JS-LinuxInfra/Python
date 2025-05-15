75bbb69174cb3ae75737c644e7ac88a08ad65290
Justin Sniesak <justindsniesak@gmail.com>
2025-05-15 11:36:09 -0500

    Update boolean_variables.py

--- boolean_variables.py
+++ boolean_variables.py
@@ -1,12 +1,12 @@
+#Define first variable
 my_baby_bool = True
+#Print first variable
 print(my_baby_bool)
+#Print first variable type
 print(type(my_baby_bool))
+#Define second variable
 my_baby_bool_two=True
+#Print second variable
 print(my_baby_bool_two)
+#Define second variable type
 print(type(my_baby_bool_two))
-
-Output:
-True
-<class 'bool'>
-True
-<class 'bool'>
