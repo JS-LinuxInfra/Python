8a3e253ba67964706f8a57e53aa056e823fe304b
Justin Sniesak <justindsniesak@gmail.com>
2025-05-15 12:00:40 -0500

    Update list_append.py

--- list_append.py
+++ list_append.py
@@ -1,8 +1,7 @@
+#Define list
 orders = ["daisies", "periwinkle"]
-
+#Append list with additional elements
 orders.append("tulips")
 orders.append("roses")
+#Print output
 print(orders)
-
-Output:
-['daisies', 'periwinkle', 'tulips', 'roses']
