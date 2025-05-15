bbc2bc4a0d1cae2b8917314cf1b5b27de8dad4a0
Justin Sniesak <justindsniesak@gmail.com>
2025-05-15 11:46:45 -0500

    Update not_statement.py

--- not_statement.py
+++ not_statement.py
@@ -1,10 +1,10 @@
+#Define boolean values and map to variable
 statement_one = False
-
 statement_two = True
-
+#Define variables
 credits = 120
 gpa = 1.8
-
+#Create not statements and print to the terminal
 if not credits >= 120:
   print("You do not have enough credits to graduate.")
 
@@ -13,6 +13,3 @@ if not gpa >= 2.0:
 
 if not (credits >= 120) and not (gpa >= 2.0):
   print("You do not meet either requirement to graduate!")
-
-Output:
-Your GPA is not high enough to graduate.
