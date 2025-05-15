7194f33c3c0e8e7e003fb8b7aea3017466a3c6d8
Justin Sniesak <justindsniesak@gmail.com>
2025-05-15 11:56:32 -0500

    Update list_elements.py

--- list_elements.py
+++ list_elements.py
@@ -1,6 +1,7 @@
+#Define variables, then pull element four
 employees = ["Michael", "Dwight", "Jim", "Pam", "Ryan", "Andy", "Robert"]
-
 employee_four = employees[3]
+#Print output with element two (Jim due to zero indexing)
 print(employees[2])
 
 Output:
