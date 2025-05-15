7a53829b9faff9f8fd86471df75d2b49202caed0
Justin Sniesak <justindsniesak@gmail.com>
2025-05-15 11:44:55 -0500

    Update or_statement.py

--- or_statement.py
+++ or_statement.py
@@ -1,12 +1,10 @@
+#Define variable and equation one
 statement_one = (2 - 1 > 3) or (-5 * 2 == -10) #True
-
+#Define variable and equation two
 statement_two = (9 + 5 <= 15) or (7 != 4 + 3) #True
-
+#Define variables
 credits = 118
 gpa = 2.0
-
+#Define if or statement and print output to the terminal
 if credits >= 120 or gpa >= 2.0:
   print("You have met at least one of the requirements.")
-
-Output:
-You have met at least one of the requirements.
