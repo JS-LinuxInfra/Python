4d4dc2ec5b74e914a4c09757216af8755219cca5
Justin Sniesak <justindsniesak@gmail.com>
2025-05-15 11:48:25 -0500

    Update else_statement.py

--- else_statement.py
+++ else_statement.py
@@ -1,10 +1,8 @@
+#Define variables
 credits = 120
 gpa = 1.9
-
+#Create if/and/else statement and print to the terminal
 if (credits >= 120) and (gpa >= 2.0):
   print("You meet the requirements to graduate!")
 else:
   print("You do not meet the requirements to graduate.")
-
-Output:
-You do not meet the requirements to graduate.
