c508425464e96bcddf3696d0339496ad2eddc5c9
Justin Sniesak <justindsniesak@gmail.com>
2025-05-15 11:59:00 -0500

    Update modify_list.py

--- modify_list.py
+++ modify_list.py
@@ -1,8 +1,8 @@
-# Your code below:
+#Define list
 garden_waitlist = ["Jiho", "Adam", "Sonny", "Alisha"]
+#Define variable using indexing
 garden_waitlist[1] = "Calla"
+#Define variable using negative indexing 
 garden_waitlist[-1] = "Alex"
+#Print output
 print(garden_waitlist)
-
-Output:
-['Jiho', 'Calla', 'Sonny', 'Alex']
