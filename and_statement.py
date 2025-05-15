1a1e329fb4aa55180e8bea8770a57e38671541a4
Justin Sniesak <justindsniesak@gmail.com>
2025-05-15 11:42:44 -0500

    Update and_statement.py

--- and_statement.py
+++ and_statement.py
@@ -1,12 +1,10 @@
+#Define variable and equation
 statement_one = (2 + 2 + 2 >= 6) and (-1 * -1 < 0) # False
-
+#Define variable and equation
 statement_two = (4 * 2 <= 8) and (7 - 1 == 6) # True
-
+#Define variables
 credits = 120
 gpa = 3.4
-
+#Create if and statement print the output to the terminal
 if credits >= 120 and gpa >=2.0:
   print("You meet the requirements to graduate!")
-
-Output:
-You meet the requirements to graduate!
