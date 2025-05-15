7256fe5385e16ef0a6d12af39c5109881acb2eca
Justin Sniesak <justindsniesak@gmail.com>
2025-05-15 11:57:34 -0500

    Update list_negative_index.py

--- list_negative_index.py
+++ list_negative_index.py
@@ -1,10 +1,8 @@
+#Define variable
 shopping_list = ["eggs", "butter", "milk", "cucumbers", "juice", "cereal"]
-
+#Use negative indexing to grab element from list
 last_element = shopping_list[-1]
 index5_element = shopping_list[5]
+#Print output
 print(last_element)
 print(index5_element)
-
-Output:
-cereal
-cereal
