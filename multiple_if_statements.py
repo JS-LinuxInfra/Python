9a18e0791bac0b2e22d665fc767f582b6d428d5d
Justin Sniesak <justindsniesak@gmail.com>
2025-05-15 11:40:38 -0500

    Update multiple_if_statements.py

--- multiple_if_statements.py
+++ multiple_if_statements.py
@@ -1,17 +1,11 @@
+#Define variables
 x = 20
 y = 20
-
-# Write the first if statement here:
+#Write the first if statement here and print output to the terminal
 if x == y:
   print("These numbers are the same")
-
-
+#Define next variable
 credits = 120
-
-# Write the second if statement here:
+#Write the second if statement and print output to the terminal
 if credits >= 120:
  print("You have enough credits to graduate!")
-
-Output:
-These numbers are the same
-You have enough credits to graduate!
