578cedf4e7f4bc75699290cc70fee07b75486f9f
Justin Sniesak <justindsniesak@gmail.com>
2025-05-15 11:37:23 -0500

    Update if_statement.py

--- if_statement.py
+++ if_statement.py
@@ -1,10 +1,7 @@
-# Enter a user name here, make sure to make it a string
+#Define first variable
 user_name = "angela_87"
-
+#Write first if statement and print outputs
 if user_name == "Dave":
   print("Get off my computer Dave!")
 if user_name == "angela_87":
   print("I know it is you, Dave! Go away!")
-
-Output:
-I know it is you, Dave! Go away!
