eb51d54e224070058a5c293e422c2170f8c3b271
Justin Sniesak <justindsniesak@gmail.com>
2025-05-31 18:07:58 -0500

    Create string_conditional.py

---
+++ string_conditional.py
@@ -0,0 +1,9 @@
+#Create function, use and with if/else statement, return boolean value based on the equation.
+def letter_check(word,letter):
+  if word == "strawberry" and letter == "a":
+    return True
+  else:
+    return False
+#Print the boolean value based on if/ese statement.
+print(letter_check("strawberry", "a"))
+print(letter_check("strawberry", "o"))
